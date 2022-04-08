6452f07007f3cb6faca318f540d1bcdce8d149be
Witcher <23rwitcher@FVFZMY2RJ1WK.tld>
2022-04-08 14:24:20 -0500

    last commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function randomNum(){
 
 
 function userNumber(){
-    let user = document.getElementById("getNumber").Value;
+    let user = document.getElementById("getNumber").value;
     return user;
 }
 
@@ -34,27 +34,27 @@ function compareNumbers(){
     let z = document.getElementById("compare");
     let c = document.getElementById("counter");
 
-    if (a!= b) {
+    if ( a != b ) {
         z.innerHTML = "Numbers are not the same. Computer got " + b + ", and user got " + a;
         z.style.color = "white";
         z.style.backgroundColor = "red";
         z.style.padding = "20px";
         z.style.textAlign = "center";
         counter++;
-        c.innerHTML = "You have tried" + counter + "times"
+        c.innerHTML = "You have tried " + counter + " times"
         c.style.color = "white";
         c.style.backgroundColor = "red";
         c.style.padding = "20px";
         c.style.textAlign = "center";
         document.querySelector("body").style.backgroundColor = "black";
 
-    }else if ( a==b ){
+    }else if ( a == b ) {
         z.innerHTML = "Numbers are the same. Computer got " + b + ", and user got " + a;
         z.style.color = "white";
         z.style.backgroundColor = "green";
         z.style.padding = "20px";
         z.style.textAlign = "center";
-        c.innerHTML = "You have tried" + counter + "times to get it right"
+        c.innerHTML = "You have tried" + counter + " times to get it right"
         c.style.color = "white";
         c.style.backgroundColor = "red";
         c.style.padding = "20px";
